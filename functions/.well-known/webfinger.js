28e49f4b9406138f164a213e195d8da4d8ef76cf
Dave Marquard <dave@marquard.org>
2022-12-06 08:03:58 -0800

    more corrections

--- functions/.well-known/webfinger.js
+++ functions/.well-known/webfinger.js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
     const resource = new URL(context.request.url).searchParams.get("resource");
     if (resource && resource.startsWith("acct:")) {
         const username = resource.substring(5);
-        return env.ASSETS.fetch(`/.well-known/webfinger/${username}.json`)
+        return context.env.ASSETS.fetch(`/.well-known/webfinger/${username}.json`)
     }
     return new Response(null, { status: 404 });
 }
