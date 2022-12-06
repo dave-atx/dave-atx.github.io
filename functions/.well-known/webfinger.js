040a1d4f67746002e12f9cc855644216f0d00614
Dave Marquard <dave@marquard.org>
2022-12-06 08:23:01 -0800

    more type fighting

--- functions/.well-known/webfinger.js
+++ functions/.well-known/webfinger.js
@@ -1,8 +1,9 @@
 export async function onRequest(context) {
-    const resource = new URL(context.request.url).searchParams.get("resource");
+    const requestURL = new URL(context.request.url);
+    const resource = requestURL.searchParams.get("resource");
     if (resource && resource.startsWith("acct:")) {
         const username = resource.substring(5);
-        return context.env.ASSETS.fetch(new Request(`/.well-known/webfinger/${username}.json`));
+        return context.env.ASSETS.fetch(new URL(`/.well-known/webfinger/${username}.json`, requestURL.origin));
     }
     return new Response(null, { status: 404 });
 }
