a95e55cb1503e0b5eeb2868c43d72ad356ee2c85
Dave Marquard <dave@marquard.org>
2022-12-06 08:17:42 -0800

    another try getting types right

--- functions/.well-known/webfinger.js
+++ functions/.well-known/webfinger.js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
     const resource = new URL(context.request.url).searchParams.get("resource");
     if (resource && resource.startsWith("acct:")) {
         const username = resource.substring(5);
-        return context.env.ASSETS.fetch(`/.well-known/webfinger/${username}.json`)
+        return context.env.ASSETS.fetch(new Request(`/.well-known/webfinger/${username}.json`));
     }
     return new Response(null, { status: 404 });
 }
